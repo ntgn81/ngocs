80f846a38a9067d83722e172b4f6a45c248fe0ac
ntgn81 <ntgn81@gmail.com>
2015-05-29 15:09:06 +0000

    Added console.log to index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ mongoose.connect(config.mongodb.connectionString, start);
 // return require('./jobs/extract-details')();
 // every 30 minutes
 function start() {
+  console.log('Connected to DB, starting scheduler');
   schedule.scheduleJob('*/10 * * * *', require('./jobs/scrape-comments'));
   schedule.scheduleJob('*/5+2 * * * *', require('./jobs/extract-details'));
   schedule.scheduleJob('*/5+4 * * * *', require('./jobs/temp-send-csv'));
