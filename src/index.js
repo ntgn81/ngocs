c5a206edcf3edc71568b9da14ff3a0e890e4aaa1
ntgn81 <ntgn81@gmail.com>
2015-05-29 15:20:20 +0000

    Updated scheduling

--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ mongoose.connect(config.mongodb.connectionString, start);
 // every 30 minutes
 function start() {
   console.log('Connected to DB, starting scheduler');
-  schedule.scheduleJob('*/10 * * * *', require('./jobs/scrape-comments'));
-  schedule.scheduleJob('*/5+2 * * * *', require('./jobs/extract-details'));
-  schedule.scheduleJob('*/5+4 * * * *', require('./jobs/temp-send-csv'));
+  // every 5 minutes
+  schedule.scheduleJob('0,5,10,15,20,25,30,35,40,45,50,55 * * * *', require('./jobs/scrape-comments'));
+  // every 5 minutes
+  schedule.scheduleJob('2,7,12,17,22,27,32,37,42,47,52,57 * * * *', require('./jobs/extract-details'));
+  // every 5 minutes
+  schedule.scheduleJob('4,9,14,19,24,29,34,39,44,49,55,59 * * * *', require('./jobs/temp-send-csv'));
 }
