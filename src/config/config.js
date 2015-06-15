eca3bb8b462a5062c58f204b7a19433cdafea381
ntgn81 <ntgn81@gmail.com>
2015-06-15 12:19:03 +0000

    Updated access token

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   instagram: {
     client_id: '42308c8fb4fc4388beaa7a1406983200',
     client_secret: '9b89915d0ff04acda526f8879fa931a1',
-    access_token: '2096526195.540da97.abba4b17ac9b414aa3ce4d8727c310fa'
+    access_token: '2096526195.540da97.6baf234bc5ac4d89a331a56ae4b2c3d6'
   },
   mongodb: {
     connectionString: 'mongodb://hidrate:hidrate@ds034878.mongolab.com:34878/iss'
