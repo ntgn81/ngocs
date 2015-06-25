8e11f5ca623d2ebfa90794b87f68db807f83e1a4
ntgn81 <ntgn81@gmail.com>
2015-06-25 15:20:56 +0000

    Fixed error

--- src/jobs/create-streak-boxes.js
+++ src/jobs/create-streak-boxes.js
@@ -6,7 +6,7 @@ var _ = require('lodash');
 var async = require('async');
 var config = require('../config/config.js');
 var Logger = require('../utils/Logger.js');
-var logger =  Logger('CreateStreakBoxes');
+var logger = new Logger('CreateStreakBoxes');
 var Streak = require('streakapi');
 
 module.exports = createStreakBoxes;
